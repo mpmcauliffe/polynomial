c15dbcd41f4f4e50a286e725293da0115a2e7f00
mpmcauliffe <mcauliffe99@gmail.com>
2019-05-02 19:25:26 -0600

    Updating LinkedList sum, but not iterating correctly-overwriting values

--- src/poly/Polynomial.java
+++ src/poly/Polynomial.java
@@ -170,30 +170,81 @@ public class Polynomial {
 		Node poly1 		 = this.poly;			// input 1
 		Node poly2 		 = p.poly;				// input 2
 		Polynomial sum   = new Polynomial(); 	// for sum
+		Node set;       //= new Node(poly1.term.coeff + poly2.term.coeff, poly1.term.degree, null); 								// build node to put into sum
 
-		//poly2.term.coeff += poly1.term.coeff;
+//		sum.poly = set;
+//		System.out.println(sum.poly.term);
+//		sum.poly = sum.poly.next;
+//		set = new Node(poly1.next.term.coeff + poly2.next.term.coeff, poly1.next.term.degree, null);
+//		sum.poly = set;
+//		System.out.println(sum.poly.term);
 
-		System.out.println(poly2.next.term.coeff);
+		while (poly1 != null || poly2 != null) {
 
-		while (poly1.next != null || poly2.next != null) {
+			if (poly1 != null && poly2 != null) {
+				/** If each term has equal degrees **/
+				if (poly1.term.degree == poly2.term.degree) {
+					/** Build node **/
+					set = new Node(poly2.term.coeff + poly1.term.coeff,
+							poly1.term.degree,
+							null);
 
 
-			poly1 = poly1.next;
-			poly2 = poly2.next;
+					sum.poly = set;            // put node into sum
+					poly1 = poly1.next;            // increment poly1
+					poly2 = poly2.next;            // increment poly2
+					System.out.println(sum.poly.term);
+
+				} else if (poly1.term.degree > poly2.term.degree) {
+					/** Only adding term from poly2 **/
+					set = new Node(poly2.term.coeff, poly2.term.degree, null);
+
+					sum.poly = set;            // put node into sum
+					poly2 = poly2.next;            // increment poly2 and only poly2
+					System.out.println(sum.poly.term);
+
+				} else {
+					/** Only adding term from poly1 **/
+					set = new Node(poly1.term.coeff, poly1.term.degree, null);
+
+					sum.poly = set;            // put node into sum
+					poly1 = poly1.next;            // increment poly1 and only poly1
+					System.out.println(sum.poly.term);
+				}
+			}
+
+			if (poly1 != null && poly2 == null) {
+				set = new Node(poly1.term.coeff, poly1.term.degree, null);
+
+				sum.poly = set;            // put node into sum
+				poly1 = poly1.next;            // increment poly1 and only poly1
+				System.out.println(sum.poly.term);
+			} else if (poly1 == null && poly2 != null) {
+				set = new Node(poly2.term.coeff, poly2.term.degree, null);
+
+				sum.poly = set;            // put node into sum
+				poly2 = poly2.next;            // increment poly2 and only poly2
+				System.out.println(sum.poly.term);
+			}
+			sum.poly = sum.poly.next;
 		}
 
-		while (poly1 != null) {
-			System.out.print(poly1.term + "  ");
-			poly1 = poly1.next;
-		}
-		System.out.print("\n");
-		while (poly2 != null) {
-			System.out.print(poly2.term + "  ");
-			poly2 = poly2.next;
-		}
-		System.out.print("\n");
+//		while (sum.poly.next != null) {
+//			System.out.print(sum.poly.term + "  ");
+//			sum.poly = sum.poly.next;
+//		}
+//		while (poly1 != null) {
+//			System.out.print(poly1.term + "  ");
+//			poly1 = poly1.next;
+//		}
+//		System.out.print("\n");
+//		while (poly2 != null) {
+//			System.out.print(poly2.term + "  ");
+//			poly2 = poly2.next;
+//		}
+//		System.out.print("\n");
 
-		return null;
+		return sum;
 	}
 	
 	/**
